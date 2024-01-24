e5ec13ea64ea72a7e995fb7977782e968844e99a
Izzy Galispie <izzyofficialworking@gmail.com>
2024-01-24 01:00:45 -0800

    ok just saving this...

--- CirclePositioner.py
+++ CirclePositioner.py
@@ -1,12 +1,19 @@
 
 def circlePositioner(radius, numNodes, startAngle = 90, **kwargs):
    strToReturn = ""
-   dAngle = 360 // numNodes
+   dAngle = 360 / numNodes
    for num in range(numNodes):
-      angle = (dAngle * num) + startAngle
-      strToReturn += "\\node[<AAAAdevtext_to_replaceAAAA>] (" + str(num) + ") at (" + str(angle) + ":" + str(radius) + ") {};\n"
-   
+      angle = ((dAngle * num) // 1) + startAngle
+      strToReturn += "\\node[<AAAAdevtext_to_replaceAAAA>] (" + str(num) + ") at (" + str(angle) + ":" + str(radius) + ") {}"
+
+      if num != 0:
+         strToReturn += "\n\tedge[<BBBBdevtext_to_replaceBBBB>] (" + str(num - 1) + ")"
+         if num == numNodes - 1:
+            strToReturn += " edge[<BBBBdevtext_to_replaceBBBB>] (0)"
+
+      strToReturn += ";\n"
+
    return strToReturn
 
 if __name__ == "__main__":
-   print(circlePositioner(8, 13))
+   print(circlePositioner(8, 14))
